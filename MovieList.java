d56b740cf41e63cf1bffee9bd20aabe4aca914e0
UNATCOcertified <83609229+UNATCOcertified@users.noreply.github.com>
2021-05-03 14:31:53 -0500

    Add files via upload

---
+++ MovieList.java
@@ -0,0 +1,21 @@
+package classproject;
+
+public class MovieList {
+
+	public MovieLibrary genresFromUser(boolean[] genreType, double minimum) {
+		
+		
+
+		return null;
+		
+	}
+	// finding the movies that satisfy one the genre inputs
+	public MovieLibrary findType(boolean[] genreType) {
+		MovieLibrary toReturn = new MovieLibrary();
+		
+		
+		return toReturn;
+		
+	}
+	
+}
